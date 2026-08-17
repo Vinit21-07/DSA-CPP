cb27266a890a9ca98414ec41a09f12e9e4a034f6
Vinit Chaudhari <vinitchaudhari2107@gmail.com>
2026-08-17 23:11:54 +0530

    first

---
+++ Game Thoery/StoneGameIX.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) 
+    {
+        int count[3] = {0, 0, 0};
+        for (int stone : stones) 
+        {
+            count[stone % 3]++;
+        }
+        if (count[0] % 2 == 0)
+        {
+            return count[1] > 0 && count[2] > 0;
+        }
+        return count[1] - count[2] > 2 || count[2] - count[1] > 2;
+    }
+};
